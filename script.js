80c3570007440dfbeb7ec6bfaef4756d9c798301
SlavikBadBoy <buracenkoslavik@gmail.com>
2024-06-17 15:37:03 +0300

    6

---
+++ script.js
@@ -0,0 +1,64 @@
+"strict mode";
+// task 1
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// task 2
+
+// for (let i = 2; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   } else {
+//     continue;
+//   }
+// }
+
+// task 3
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(7 * i);
+// }
+
+// task 4
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// let i = 0;
+// while (i < numbers.length) {
+//   console.log(numbers[i]);
+//   i++;
+// }
+
+// task 5
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] === 7) {
+//     break;
+//   }
+//   console.log(numbers[i]);
+// }
+
+// task 6
+
+// let n = 15;
+// let i = 1;
+// while (i < n) {
+//   console.log(i);
+//   i++;
+// }
+
+// task 7
+
+// let i = 1;
+// while (i <= 20) {
+//   if (i % 3 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+//   i++;
+// }
